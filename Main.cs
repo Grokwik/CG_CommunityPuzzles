9c479d11bd44ae398bea508de6e5a07f2c672d22
Grokwik <frederic@bodin.site>
2017-02-10 20:03:09 +0100

    These romans are crazy

--- Main.cs
+++ Main.cs
@@ -4,13 +4,15 @@ namespace Project
 {
     class Solution
     {
-        static void Main(string[] args)
-        {
-//            int N = int.Parse(Console.ReadLine());
-//            Console.WriteLine(TheTwoEggs.Solution.MinTries(N));
+        /*
+         *        static void Main(string[] args)
+                {
+        //            int N = int.Parse(Console.ReadLine());
+        //            Console.WriteLine(TheTwoEggs.Solution.MinTries(N));
 
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine(MagicStones.Solution.WorkYourMagic(n, Console.ReadLine()));
-        }
+                    int n = int.Parse(Console.ReadLine());
+                    Console.WriteLine(MagicStones.Solution.WorkYourMagic(n, Console.ReadLine()));
+                }
+        */
     }
 }
